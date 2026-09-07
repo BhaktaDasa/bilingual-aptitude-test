2f93183207337ee8c263f0cf4a888aa11a02cbe4
BhaktaDasa <bhaktadas12345@gmail.com>
2026-09-07 15:29:36 +0530

    fix: remove undefined window references breaking script initialization and bump service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for 100% Offline Aptitude Testing
-const CACHE_NAME = 'aptitude-master-v2';
+const CACHE_NAME = 'aptitude-master-v3';
 const STATIC_ASSETS = [
   './',
   './index.html',
